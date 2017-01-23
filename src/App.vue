d4e61dce7426870deccc334ee531b13ad1000dd1
gabs159 <gabe94@bitbucket.org>
2017-01-23 19:46:07 -0200

    initial commit of vue-form

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div id="app">
+<h1>Formulário Polissono</h1>
+<formulario></formulario>
+</div>
+</template>
+
+<script>
+import Formulario from './components/Formulario'
+
+export default {
+  name: 'app',
+  components: {
+    Formulario
+  }
+}
+
+</script>
+
+<style>
+#app {
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 20px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  }
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,50 @@
+  <!--
+  Colocar mascara nos campos, data de nascimento, estatura, peso, telefones.
+   -->
+<template>
+<div id=app>
+
+
+
+  <div id=form>
+
+    <p>Nome Completo: <input v-model="name"><br>
+      <p>
+        <p>Data de Nascimento: <input v-model="message" placeholder="__/__/__"></p>
+        <p>
+          <form id="gender">
+            Sexo:
+            <input type="radio" id="f" value="F" v-model="picked" v-bind:false-value="a">
+            <label for="F">F</label>
+
+            <input type="radio" id="m" value="M" v-model="picked" v-bind:false-value="b">
+            <label for="M">M</label>
+            <br>
+          </form>
+        </p>
+        <p>Peso: <input v-model="peso"><br></p>
+        <p>Estatura: <input v-model="estatura"></p>
+        <p>Telefones para contato: <input type=text></p>
+        <p>Medicamentos que faz uso regular:</p>
+        <textarea v-model="medicamentos" placeholder="Medicamentos que faz uso regular."></textarea>
+
+  </div>
+
+</div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#form {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 20px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
